a9a9682b989d1e128714076043e14461d65604b8
Vaishnavi <vaishnavi.jagtap_ma2025@ashoka.edu.in>
2026-05-14 13:43:00 +0530

    Update __init__.py

--- project_code/__init__.py
+++ project_code/__init__.py
@@ -1,1 +1,19 @@
-"""Helper package for reusable project code."""
+"""
+Reusable helper functions for the Indian Equity Price Predictor project.
+"""
+
+from .template_outputs import (
+    build_baseline_metric,
+    build_primary_metric,
+    build_milestone_manifest,
+)
+
+from .io import write_json, read_json
+
+__all__ = [
+    "build_baseline_metric",
+    "build_primary_metric",
+    "build_milestone_manifest",
+    "write_json",
+    "read_json",
+]
